a19fcc872ddd561b372f5e05986f236ad76c05e5
Golamrabbi Azad <golamrabbiazad@gmail.com>
2023-12-12 01:10:17 +0600

    fix: <Portal/ > was missing

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,5 +1,6 @@
 import { createFileUploader } from "@solid-primitives/upload";
 import { For, Setter, createSignal } from "solid-js";
+import { Portal } from "solid-js/web";
 
 export function Modal({ setIsOpen }: { setIsOpen: Setter<boolean> }) {
   const { files, selectFiles } = createFileUploader({
@@ -10,130 +11,132 @@ export function Modal({ setIsOpen }: { setIsOpen: Setter<boolean> }) {
   const [isUploaded, setIsUploaded] = createSignal(false);
 
   return (
-    <div
-      class="relative z-10"
-      aria-labelledby="modal-title"
-      role="dialog"
-      aria-modal="true"
-    >
-      <div class="fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity" />
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <div class="sm:flex sm:items-start">
-                <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6 text-gray-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path d="M19.906 9c.382 0 .749.057 1.094.162V9a3 3 0 00-3-3h-3.879a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H6a3 3 0 00-3 3v3.162A3.756 3.756 0 014.094 9h15.812zM4.094 10.5a2.25 2.25 0 00-2.227 2.568l.857 6A2.25 2.25 0 004.951 21H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-2.227-2.568H4.094z" />
-                  </svg>
-                </div>
-                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <h3
-                    class="text-base font-semibold leading-6 text-gray-900"
-                    id="modal-title"
-                  >
-                    Attach Document
-                  </h3>
-                  <div class="mt-2">
-                    <div class="flex justify-center w-[400px]">
-                      <div class="flex items-center justify-center rounded-lg border-4 border-dashed h-32 p-10 w-96">
-                        <div
-                          class="bg-slate-100 w-16 h-16 flex items-center justify-center rounded-full cursor-pointer"
-                          onclick={() => {
-                            selectFiles((files) =>
-                              files.forEach((file) => console.log(file))
-                            );
-
-                            setIsUploaded(true);
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6 cursor-pointer"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                            />
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
+    <Portal>
+      <div
+        class="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div class="fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity" />
+        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                  <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6 text-gray-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M19.906 9c.382 0 .749.057 1.094.162V9a3 3 0 00-3-3h-3.879a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H6a3 3 0 00-3 3v3.162A3.756 3.756 0 014.094 9h15.812zM4.094 10.5a2.25 2.25 0 00-2.227 2.568l.857 6A2.25 2.25 0 004.951 21H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-2.227-2.568H4.094z" />
+                    </svg>
                   </div>
-                  <div class="mt-4">
-                    <For each={files()}>
-                      {(item) => (
-                        <div class="flex gap-2 items-center p-2">
-                          {item.file.type === "image/png" ? (
-                            <img
-                              src={item.source}
-                              alt={item.name}
-                              class="w-6 h-6"
-                            />
-                          ) : (
+                  <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h3
+                      class="text-base font-semibold leading-6 text-gray-900"
+                      id="modal-title"
+                    >
+                      Attach Document
+                    </h3>
+                    <div class="mt-2">
+                      <div class="flex justify-center w-[400px]">
+                        <div class="flex items-center justify-center rounded-lg border-4 border-dashed h-32 p-10 w-96">
+                          <div
+                            class="bg-slate-100 w-16 h-16 flex items-center justify-center rounded-full cursor-pointer"
+                            onclick={() => {
+                              selectFiles((files) =>
+                                files.forEach((file) => console.log(file))
+                              );
+
+                              setIsUploaded(true);
+                            }}
+                          >
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
                               stroke-width="1.5"
                               stroke="currentColor"
-                              class="w-6 h-6"
+                              class="w-6 h-6 cursor-pointer"
                             >
                               <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
                               />
                             </svg>
-                          )}
-                          <p class="text-gray-700 font-bold">{item.name}</p>
+                          </div>
                         </div>
-                      )}
-                    </For>
+                      </div>
+                    </div>
+                    <div class="mt-4">
+                      <For each={files()}>
+                        {(item) => (
+                          <div class="flex gap-2 items-center p-2">
+                            {item.file.type === "image/png" ? (
+                              <img
+                                src={item.source}
+                                alt={item.name}
+                                class="w-6 h-6"
+                              />
+                            ) : (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                                />
+                              </svg>
+                            )}
+                            <p class="text-gray-700 font-bold">{item.name}</p>
+                          </div>
+                        )}
+                      </For>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                class="inline-flex w-full justify-center rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 sm:ml-3 sm:w-auto"
-                onclick={() => {
-                  window.localStorage.setItem(
-                    "attachments",
-                    JSON.stringify(files())
-                  );
+              <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  class="inline-flex w-full justify-center rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 sm:ml-3 sm:w-auto"
+                  onclick={() => {
+                    window.localStorage.setItem(
+                      "attachments",
+                      JSON.stringify(files())
+                    );
 
-                  isUploaded() ? setIsOpen(false) : true;
-                }}
-              >
-                Upload
-              </button>
-              <button
-                type="button"
-                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                onclick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button>
+                    isUploaded() ? setIsOpen(false) : true;
+                  }}
+                >
+                  Upload
+                </button>
+                <button
+                  type="button"
+                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  onclick={() => setIsOpen(false)}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Portal>
   );
 }
